7b05d7bef1a8363846aea38aee35c147716c969f
LpxsBr <anselmolopes.an@gmail.com>
2023-03-19 14:02:38 -0300

    add localhost

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,5 +4,5 @@ module.exports = {
     dbHost: String(process.env.MONGODB_URI),
     dbName: String(process.env.DB_NAME),
     serverPort: Number(process.env.PORT),
-    frontCLient: String(process.env.FRONT_CLIENT)
+    frontCLient: String(process.env.FRONT_CLIENT)  || 'http://localhost:8080'
 }
